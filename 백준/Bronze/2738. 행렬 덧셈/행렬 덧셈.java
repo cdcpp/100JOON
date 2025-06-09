5418829ab8061fb4b374af77196083ef24663aa6
ministar <48432289+cdcpp@users.noreply.github.com>
2025-06-09 21:19:58 +0900

    [Bronze III] Title: 행렬 덧셈, Time: 484 ms, Memory: 36680 KB -BaekjoonHub

---
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args)  {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        int [][] arr1 = new int[a][b];
+        int [][] arr2 = new int[a][b];
+        int [][] arr3 = new int[a][b];
+
+
+        for(int i = 0;i < a;i++){
+            for(int j = 0;j < b;j++){
+                arr1[i][j] = sc.nextInt();
+            }
+        }
+
+        for(int i = 0;i < a;i++){
+            for(int j = 0;j < b;j++){
+                arr2[i][j] = sc.nextInt();
+            }
+        }
+
+        for(int i = 0;i < a;i++){
+            for(int j = 0;j < b;j++){
+                arr3[i][j] = arr1[i][j] + arr2[i][j];
+            }
+        }
+
+        for(int i = 0;i < a;i++){
+            for(int j = 0;j < b;j++){
+                System.out.print(arr3[i][j] + " ");
+            }
+            System.out.println();
+        }
+        
+    }
+}
